e46cfdd3e4271332da2801b6e3943335188b7fd1
曹檑 <1090491594@qq.com>
2017-07-24 15:57:48 +0800

    Initial Commit

---
+++ 推流test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  推流test
+//
+//  Created by Zeus on 2017/7/24.
+//  Copyright © 2017年 Zeus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 推流test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  推流test
+//
+//  Created by Zeus on 2017/7/24.
+//  Copyright © 2017年 Zeus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
